70cc5fd8d9c1d72b50fedbde8f52396ec272d644
Abhishek Rana <40822125+AbhishekRana23@users.noreply.github.com>
2021-07-25 13:32:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask , jsonify
 from covid import Covid
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-
+CORS(app)
 covid = Covid(source="worldometers")
 
 @app.route('/')
